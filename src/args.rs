deacb107b77cbfbc048f6de38b4ccc6ed1b0747a
CarbonFlora <zihaoliang0413@gmail.com>
2023-07-27 14:06:14 -0700

    updated version

--- src/args.rs
+++ src/args.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 /// Simple program to verify workbook data integrity.
 #[derive(Parser, Debug)]
-#[command(author="Zi Hao L.", version="0.2.0", about="Workbook Data Integrity Check.", long_about = None)]
+#[command(author="Zi Hao L.", version="1.0.0", about="Workbook Data Integrity Check.", long_about = None)]
 pub struct DIFloraArgs {
     // /// Print duplicates or conflicts
     // #[arg(short, long, default_value_t = false)]
