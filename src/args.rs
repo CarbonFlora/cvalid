5a73848ba08081fd82eadcaa89c590eeae94900f
CarbonFlora <zihaoliang0413@gmail.com>
2023-08-08 15:12:43 -0700

    version update

--- src/args.rs
+++ src/args.rs
@@ -2,14 +2,14 @@ use clap::Parser;
 
 /// Simple program to verify workbook data integrity.
 #[derive(Parser, Debug)]
-#[command(author="Zi Hao L.", version="1.0.2", about="Workbook Data Integrity Check.", long_about = None)]
+#[command(author="Zi Hao L.", version="1.1.0", about="Workbook Data Integrity Check.", long_about = None)]
 pub struct DIFloraArgs {
     /// Specify a specific column
-    #[arg(short, long, default_value_t = 5)]
+    #[arg(short, long, default_value_t = 6)]
     pub column: usize,
 
     /// Specify number of beginning rows to skip
-    #[arg(short, long, default_value_t = 2)]
+    #[arg(short, long, default_value_t = 3)]
     pub rskip: usize,
 
     /// Specify number of beginning worksheets to skip
